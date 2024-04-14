2c4faa15d5d490b1639f2a69ced26e2923cc665c
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-14 19:19:33 +0600

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Home from "./Pages/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <>
+    <RouterProvider router={router}>
       <Home />
-    </>
+    </RouterProvider>
   );
 }
 
