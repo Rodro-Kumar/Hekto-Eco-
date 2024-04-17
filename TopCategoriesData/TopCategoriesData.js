3d4bd84f2f6d893d0eee5b4a894e2a2ba8b7fb1e
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-17 12:27:19 +0600

    top categories data

---
+++ TopCategoriesData/TopCategoriesData.js
@@ -0,0 +1,81 @@
+import img1 from "../src/assets/top1.png";
+import img2 from "../src/assets/top2.png";
+import img3 from "../src/assets/top3.png";
+import img4 from "../src/assets/top1.png";
+
+const TopCategoriesData = [
+  {
+    id: 1,
+    ImG: img1,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 2,
+    ImG: img2,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 3,
+    ImG: img4,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 4,
+    ImG: img3,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 5,
+    ImG: img1,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 6,
+    ImG: img2,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 7,
+    ImG: img3,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 8,
+    ImG: img4,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 9,
+    ImG: img2,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 10,
+    ImG: img1,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 11,
+    ImG: img3,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+  {
+    id: 12,
+    ImG: img4,
+    title: "Mini LCW Chair",
+    price: "$56.00",
+  },
+];
+
+export default TopCategoriesData;
