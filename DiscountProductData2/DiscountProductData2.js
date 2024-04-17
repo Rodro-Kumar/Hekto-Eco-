b89738abd6702e31394dcc02e9d759ca93a428f4
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-17 12:26:44 +0600

    discount data

---
+++ DiscountProductData2/DiscountProductData2.js
@@ -0,0 +1,11 @@
+import img1 from "../src/assets/1.png";
+import img2 from "../src/assets/2.png";
+import img3 from "../src/assets/3.png";
+
+const discountData2 = [
+  { id: 1, img: img1, title: "Executive Seat chair", price: "$52.00" },
+  { id: 2, img: img2, title: "Executive Seat chair", price: "$32.00" },
+  { id: 3, img: img3, title: "Executive Seat chair", price: "$42.00" },
+];
+
+export default discountData2;
