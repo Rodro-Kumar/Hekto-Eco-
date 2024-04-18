6a5df9e4fcf86bc5214f85d65466096dd8e399e4
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-18 11:57:37 +0600

    discount item

---
+++ src/HomeComponent/DiscountItem/DiscountItem.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import WoodChair from "../../CommonComponent/WoodChair/WoodChair";
+import { Link } from "react-router-dom";
+
+const DiscountItem = () => {
+  return (
+    <>
+      <div className="pb-20 px-4 lg:px-0">
+        <div className="container">
+          <div>
+            <h2 className="text-[#151875] font-JosfinSans font-bold text-4xl sm:text-5xl pb-8 text-center">
+              Discount Item
+            </h2>
+            <div className="item flex items-center justify-center gap-x-5 text-[#151875] font-Lato font-normal text-base sm:text-lg">
+              <Link>Wood Chair</Link>
+              <Link>Plastic Chair</Link>
+              <Link>Sofa Colletion</Link>
+            </div>
+            <div>
+              <WoodChair />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DiscountItem;
