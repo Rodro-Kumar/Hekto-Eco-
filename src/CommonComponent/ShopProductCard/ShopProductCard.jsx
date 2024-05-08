ed5debcc6b288f673a2a30232f4ef6d81cd4f5d7
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-05-08 12:11:54 +0600

    responsive && props

--- src/CommonComponent/ShopProductCard/ShopProductCard.jsx
+++ src/CommonComponent/ShopProductCard/ShopProductCard.jsx
@@ -3,12 +3,18 @@ import { IoIosCart } from "react-icons/io";
 import { FaSearchengin } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 
-const ShopProductCard = ({ productImg }) => {
+const ShopProductCard = ({
+  productImg,
+  productTitle,
+  mainPrice,
+  discount,
+  discountStatus,
+}) => {
   return (
     <>
-      <div className="w-[270px] h-[363px] cursor-pointer group">
+      <div className="shopProductcard w-[162px] md:w-[270px] h-[258px] md:h-[363px] cursor-pointer group">
         <div className="relative overflow-hidden">
-          <div className="bg-[#F6F7FB] h-[280px] group-hover:bg-[#EBF4F3] flex items-center justify-center">
+          <div className="bg-[#F6F7FB] h-[166px] md:h-[280px] group-hover:bg-[#EBF4F3] flex items-center justify-center">
             <img
               src={productImg ? productImg : null}
               alt={productImg}
@@ -29,21 +35,23 @@ const ShopProductCard = ({ productImg }) => {
         </div>
 
         <div className="pt-3 flex flex-col items-center justify-center">
-          <h4 className="text-lg font-JosfinSans font-bold text-[#151875] pb-2">
-            Vel elit euismod
+          <h4 className="text-lg font-JosfinSans font-bold text-[#151875] pb-2 text-ellipsis whitespace-nowrap overflow-hidden text-center w-full">
+            {productTitle ? productTitle : "Vel elit euismod "}
           </h4>
           <div className="flex items-center gap-x-[6px]">
             <div className="w-[10px] h-[10px] bg-[#DE9034] rounded-full"></div>
-            <div className="w-[10px] h-[10px] bg-[#DE9034] rounded-full"></div>
-            <div className="w-[10px] h-[10px] bg-[#DE9034] rounded-full"></div>
+            <div className="w-[10px] h-[10px] bg-[#EC42A2] rounded-full"></div>
+            <div className="w-[10px] h-[10px] bg-[#8568FF] rounded-full"></div>
           </div>
           <div className="flex items-center gap-x-[10px] pt-3">
             <span className="text-[#151875] text-sm font-JosfinSans font-normal">
-              $26.00{" "}
+              {mainPrice ? mainPrice : "$00.00"}
             </span>
-            <span className="text-[#FB2E86] text-sm font-JosfinSans font-normal">
-              $42.00{" "}
-            </span>
+            {discountStatus && (
+              <span className="text-[#FB2E86] text-sm font-JosfinSans font-normal line-through">
+                {discount ? discount : "$00.00"}
+              </span>
+            )}
           </div>
         </div>
       </div>
